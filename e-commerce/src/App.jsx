fbee375ca346e6a6f87a16687424adb37975f540
Loicet <l.teta2@alustudent.com>
2025-11-05 15:59:25 +0200

    Added the app.jsx file

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,13 +1,28 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
-
-
   return (
-    <>
-     <p className='text-red-400'>help</p>
-    </>
-  )
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
